0ccbc5ea5f037c46aa121a817a21b6b68a058600
ashel111 <21h09.ashel@sjec.ac.in>
2023-06-01 12:18:25 +0530

    first commit

---
+++ vviews/AmesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmesApp.swift
+//  Ames
+//
+//  Created by Ashel D Souza on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AmesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
